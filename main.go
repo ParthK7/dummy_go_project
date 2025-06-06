e3a0e8ab5746a384dae4ec897aeb850690519a47
ParthK7 <parthkulkarni15@gmail.com>
2025-06-06 16:39:36 -0400

    Changed Dockerfile

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -13,7 +12,7 @@ func main() {
 
 	m.HandleFunc("/", handlePage)
 
-	port := os.Getenv("PORT")
+	const port = "8010"
 	srv := http.Server{
 		Handler:      m,
 		Addr:         ":" + port,
@@ -34,7 +33,7 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	const page = `<html>
 <head></head>
 <body>
-	<p> Hello from Docker! I'm a Go server. </p>
+	<p> Hi Docker, I pushed a new version. </p>
 </body>
 </html>
 `
